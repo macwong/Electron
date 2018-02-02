e7f4a89af3507156a22d9ff1712dd79cee121452
Dave Mac <ihategates@hotmail.com>
2018-02-02 14:39:05 +1100

    Another test etc

--- Boilerplate/app/components/Home.js
+++ Boilerplate/app/components/Home.js
@@ -11,10 +11,12 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+        <h2>Test</h2>
+        <video id="camera" autoplay> </video>
+        {/* <div id="camdemo" style="width: 320px; height: 240px; text-align: center; margin: 0 auto;"></div>
+        <div style="text-align:center;">
+            <input type="button" id="start" value="Start / Shut down camera"/>
+        </div> */}
       </div>
     );
   }
