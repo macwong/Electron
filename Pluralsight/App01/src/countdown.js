f0cbfd209ff4f876bd10eaf8723061bea2c54768
Dave Mac <dave.a.mccormick@gmail.com>
2018-02-06 14:25:31 +1100

    Working countdown

--- Pluralsight/App01/src/countdown.js
+++ Pluralsight/App01/src/countdown.js
@@ -1,9 +1,9 @@
 module.exports = function countdown(tick) {
-    let count = 10;
+    let count = 3;
 
     let timer = setInterval(() => {
         tick(count--);
-        
+
         if (count < 0) {
             clearInterval(timer);
         }
