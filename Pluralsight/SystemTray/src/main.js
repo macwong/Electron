6e1d1ac0c594d3da69ff6096c6a652c162804c8a
Dave Mac <dave.a.mccormick@gmail.com>
2018-02-06 15:55:56 +1100

    system tray and clipboard

--- Pluralsight/SystemTray/src/main.js
+++ Pluralsight/SystemTray/src/main.js
@@ -1,4 +1,22 @@
-electron = require('electron');
-Tray = electron.Tray;
+const electron = require('electron');
+const path = require('path');
 
-new Tray();
+const { app, Tray, Menu } = electron;
+
+const myPath = path.join('src', 'icon.jpg');
+
+app.on('ready', () => {
+    let tray = new Tray(myPath);
+
+    let trayMenu = Menu.buildFromTemplate([
+        {
+            label: "Wow",
+            click: () => {
+                console.log('Wowser');
+            }
+        }
+    ]);
+
+    tray.setContextMenu(trayMenu);
+    tray.setToolTip("This is a test");
+});
